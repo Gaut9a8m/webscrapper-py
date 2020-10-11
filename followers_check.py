25d1f87609e6b8dffdf1034e50390def1d621e9a
Gaut9a8m <gaut9a8m@gmail.com>
2020-10-11 15:20:08 +0530

    minor changes

--- followers_check.py
+++ followers_check.py
@@ -5,7 +5,7 @@ import signal
 import time 
 
 
-url = 'https://www.instagram.com/khane_p_kharcha/'
+url = 'https://www.instagram.com/YOUR USER NAME'
 headers={"User-Agent":'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36'} 
 
 
